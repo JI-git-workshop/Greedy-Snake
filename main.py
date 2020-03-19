72ca83faccf929c9850190d6c9ca857c01eda397
Lzx-surface <1174184337@qq.com>
2020-03-19 09:53:34 +0800

    add basic functions

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+from random import randrange
+
+def randomFood(food):
+    food.left = randrange(0, 600, 10)
+    food.top = randrange(0, 400, 10)
 
 
 pygame.init()
@@ -9,44 +14,63 @@ screen = pygame.display.set_mode((600, 400), 0, 32)
 
 pygame.display.set_caption("hello world")
 
-moveX = 0
-moveY = 0
-# speed = [0, 0]
-# acceleration = 1
-pos = [300, 200]
+# index: 0 for x-axis, 1 for y-axis
+direct = [1, 0]
+
+body = [Rect(300, 200, 10, 10)]
+food = Rect(200, 100, 10, 10)
 # aBlock = pygame.draw.rect(screen, Color(0, 255, 0, 255), (pos[0], pos[1], 10, 10), 0)
 
 while True:
-    pygame.time.delay(20)
+    pygame.time.delay(100)
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
 
         if event.type == KEYDOWN:
             if event.key == K_LEFT:
-                moveX = -1
-                # aBlock.move(-10, 0)
+                if direct[0] ==0:
+                    direct[0] = -1
+                    direct[1] = 0
             elif event.key == K_RIGHT:
-                moveX = 1
-                # aBlock.move(10, 0)
+                if direct[0] ==0:
+                    direct[0] = 1
+                    direct[1] = 0
             elif event.key == K_UP:
-                moveY = -1
-                # aBlock.move(0, 10)
+                if direct[1] ==0:
+                    direct[1] = -1
+                    direct[0] = 0
             elif event.key == K_DOWN:
-                moveY = 1
-                # aBlock.move(0, -10)
-        elif event.type == KEYUP:
-            if event.key == K_LEFT or event.key == K_RIGHT:
-                moveX = 0
-            elif event.key == K_UP or event.key == K_DOWN:
-                moveY = 0
+                if direct[1] ==0:
+                    direct[1] = 1
+                    direct[0] = 0
+        # elif event.type == KEYUP:
+        #     if event.key == K_LEFT or event.key == K_RIGHT:
+        #         direct[0] = 0
+        #     elif event.key == K_UP or event.key == K_DOWN:
+        #         direct[1] = 0
 
-   
-    pos[0] += moveX * 2
-    pos[1] += moveY * 2
+    if len(body) == 1:
+        body[0].centerx += direct[0] * 10
+        body[0].centery += direct[1] * 10
+        if (body[0].colliderect(food)):
+            body.append(Rect(body[0].left - direct[0]*10, body[0].top - direct[1]*10, 10, 10))
+            randomFood(food)
+    else:
+        if food.collidepoint(body[0].centerx + direct[0] * 10, body[0].centery + direct[1] * 10):
+            newHead = Rect(food.left, food.top, 10, 10)
+            body.insert(0, newHead)
+            randomFood(food)
+        else:
+            body[-1].left = body[0].left + direct[0] * 10
+            body[-1].top = body[0].top + direct[1] * 10
+            body.insert(0, body[-1])
+            body.pop(-1)
+            
     
     screen.fill((0, 0, 0))
-    # aBlock.move(pos[0], pos[1])
-    pygame.draw.rect(screen, Color(0, 255, 0, 255), (pos[0], pos[1], 10, 10), 0)
+    for aRect in body:
+        pygame.draw.rect(screen, Color(0, 255, 0, 255), aRect, 0)
+    pygame.draw.rect(screen, Color(255,255,255,255), food)
 
     pygame.display.update()
