6d752b6a0b0ace7556c126a736e373cf323a41e3
Lzx-surface <1174184337@qq.com>
2020-03-23 22:17:37 +0800

    backup before BIG change

--- main.py
+++ main.py
@@ -2,10 +2,33 @@ import pygame
 from pygame.locals import *
 from sys import exit
 from random import randrange
+from snake import *
 
-def randomFood(food):
-    food.left = randrange(0, 600, 10)
-    food.top = randrange(0, 400, 10)
+pixelX = 900
+pixelY = 600
+length = 15
+
+
+def randomFood(aRect):
+    aRect.left = randrange(0, pixelX, length)
+    aRect.top = randrange(0, pixelY, length)
+    return aRect
+
+class foodClass:
+    state = [-1, 0, 0, 1, 1, 1, 1, 0, 0, -1]
+    def __init__(self):
+        self.image = pygame.image.load("Strawberry-15x15.png")
+        self.collideBox = randomFood(Rect(200, 100, length, length))
+        self.time = 2
+    
+    def collidepoint(self, x, y):
+        return self.collideBox.collidepoint(x, y)
+
+    def show(self, screen):
+        self.time += 1
+        self.time %= 10
+        screen.blit(self.image,(self.collideBox.left, self.collideBox.top + self.state[self.time]))
+
 
 def isDead(body):
     head = body[0]
@@ -17,16 +40,15 @@ def isDead(body):
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 400), 0, 32)
+screen = pygame.display.set_mode((pixelX, pixelY), 0, 32)
 
 pygame.display.set_caption("Greedy Snake Ver-1.0")
 
 # index: 0 for x-axis, 1 for y-axis
 direct = [1, 0]
 
-body = [Rect(300, 200, 10, 10)]
-food = Rect(200, 100, 10, 10)
-randomFood(food)
+body = [Rect(randrange(0, pixelX, length), randrange(0, pixelY, length), length, length)]
+food = foodClass()
 
 
 inputCount = 0
@@ -62,26 +84,26 @@ while True:
                     inputCount += 1
 
     if len(body) == 1:
-        body[0].centerx += direct[0] * 10
-        body[0].centery += direct[1] * 10
+        body[0].left += direct[0] * length
+        body[0].top += direct[1] * length
 
-        body[0].centerx %= 600
-        body[0].centery %= 400
+        body[0].left %= pixelX
+        body[0].top %= pixelY
 
-        if (body[0].colliderect(food)):
-            body.append(Rect(body[0].left - direct[0]*10, body[0].top - direct[1]*10, 10, 10))
-            randomFood(food)
+        if (body[0].colliderect(food.collideBox)):
+            body.append(Rect(body[0].left - direct[0]*length, body[0].top - direct[1]*length, length, length))
+            randomFood(food.collideBox)
     else:
-        if food.collidepoint(body[0].centerx + direct[0] * 10, body[0].centery + direct[1] * 10):
-            newHead = Rect(food.left, food.top, 10, 10)
+        if food.collidepoint(body[0].left + direct[0] * length, body[0].top + direct[1] * length):
+            newHead = Rect(food.collideBox.left, food.collideBox.top, length, length)
             body.insert(0, newHead)
-            randomFood(food)
+            randomFood(food.collideBox)
         else:
-            body[-1].left = body[0].left + direct[0] * 10
-            body[-1].top = body[0].top + direct[1] * 10
+            body[-1].left = body[0].left + direct[0] * length
+            body[-1].top = body[0].top + direct[1] * length
 
-            body[-1].left %= 600
-            body[-1].top %= 400
+            body[-1].left %= pixelX
+            body[-1].top %= pixelY
 
 
             body.insert(0, body[-1])
@@ -94,6 +116,7 @@ while True:
     screen.fill((0, 0, 0))
     for aRect in body:
         pygame.draw.rect(screen, Color(0, 255, 0, 255), aRect, 0)
-    pygame.draw.rect(screen, Color(255,255,255,255), food)
+    # pygame.draw.rect(screen, Color(255,255,255,255), food)
+    food.show(screen)
 
     pygame.display.update()
---
+++ snake.py
@@ -0,0 +1,162 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+from random import randrange
+
+class bodyGeneral:
+    def __init__(self, component = None, direction = [1, 0]):
+        self.component = component
+        self.image = None
+        if component != None:
+            self.image = pygame.image.load(component + ".png")
+        
+        self.collideBox = Rect(0, 0, 15, 15)
+        self.direction = direction
+
+    def move(self, direction):
+        originalDirection = self.direction[:]
+
+        self.direction = direction[:]
+        self.collideBox.left += direction[0] * 15
+        self.collideBox.top += direction[1] * 15
+        return originalDirection
+
+        
+    
+    def show(self, screen):
+        temp = self.image
+        if self.component == "head":
+            # store the angle of rotation
+            rotation = [180, 90, 0, -90, 0]
+            temp = pygame.transform.rotate(self.image, rotation[self.direction[0] * 2 + self.direction[1] + 2])
+        elif self.component == "tail":
+            rotation = [0, -90, 0, 90, 180]
+            temp = pygame.transform.rotate(self.image, rotation[self.direction[0] * 2 + self.direction[1] + 2])
+        elif self.component == "body":
+            rotation = [180, 90, 0, -90, 0]
+            temp = pygame.transform.rotate(self.image, rotation[self.direction[0] * 2 + self.direction[1] + 2])
+
+            temp = pygame.transform.rotate(temp, 180 * self.state)
+            self.state = 1 - self.state
+
+        screen.blit(temp,(self.collideBox.left, self.collideBox.top))
+
+
+class bodyS(bodyGeneral):
+    def __init__(self, direction = [1,0]):
+        bodyGeneral.__init__(self, "body", direction)
+        # 0: up  1: down
+        self.state = 0
+
+class snake:
+    def __init__(self):
+        self.body = [bodyGeneral("head", [1, 0]), bodyGeneral("tail", [1, 0])]
+        self.body[0].collideBox.left = 300
+        self.body[0].collideBox.top = 300
+        self.body[1].collideBox.left = 285
+        self.body[1].collideBox.top = 300
+    
+    def move(self, direction):
+        for i in self.body:
+            direction = i.move(direction)[:]
+            # TODO When turning the direction is special
+            # self.body[1].move(direction1)
+
+    def show(self, screen):
+        for i in self.body:
+            i.show(screen)
+
+    def append(self):
+        tail = self.body[-1]
+        newBody = bodyS(tail.direction[:])
+        newBody.collideBox.left = tail.collideBox.left
+        newBody.collideBox.top = tail.collideBox.top
+
+        if len(self.body) >= 3:
+            newBody.state = 1 - self.body[-2].state
+        self.body.insert(-1, newBody)
+        tail.collideBox.left -= tail.direction[0] * 15
+        tail.collideBox.top -= tail.direction[1] * 15
+
+
+
+
+if __name__ == "__main__":
+    pixelX = 900
+    pixelY = 600
+    length = 15
+
+    pygame.init()
+    pygame.display.set_icon(pygame.image.load("Title-Image.png"))
+
+    screen = pygame.display.set_mode((pixelX, pixelY), 0, 32)
+
+    pygame.display.set_caption("Greedy Snake Ver-1.0")
+
+    # index: 0 for x-axis, 1 for y-axis
+    direct = [1, 0]
+
+    body = snake()
+
+
+    inputCount = 0
+    while True:
+        inputCount = 0
+        pygame.time.delay(250)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+
+            if event.type == KEYDOWN:
+                if inputCount == 1:
+                    continue
+                if event.key == K_LEFT:
+                    if direct[0] == 0:
+                        direct[0] = -1
+                        direct[1] = 0
+                        inputCount += 1
+                elif event.key == K_RIGHT:
+                    if direct[0] == 0:
+                        direct[0] = 1
+                        direct[1] = 0
+                        inputCount += 1
+                elif event.key == K_UP:
+                    if direct[1] == 0:
+                        direct[1] = -1
+                        direct[0] = 0
+                        inputCount += 1
+                elif event.key == K_DOWN:
+                    if direct[1] == 0:
+                        direct[1] = 1
+                        direct[0] = 0
+                        inputCount += 1
+                elif event.key == K_m:
+                    body.append()
+
+        # if len(body) == 1:
+        #     body[0].left += direct[0] * length
+        #     body[0].top += direct[1] * length
+
+        #     body[0].left %= pixelX
+        #     body[0].top %= pixelY
+
+        # else:
+            # body[-1].left = body[0].left + direct[0] * length
+            # body[-1].top = body[0].top + direct[1] * length
+
+            # body[-1].left %= pixelX
+            # body[-1].top %= pixelY
+
+
+            # body.insert(0, body[-1])
+            # body.pop(-1)
+        body.move(direct)
+                
+        screen.fill((0, 0, 0))
+
+        body.show(screen)
+        # for aRect in body:
+        #     pygame.draw.rect(screen, Color(0, 255, 0, 255), aRect, 0)
+
+        pygame.display.update()
+
